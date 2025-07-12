21aa2d6b5c141e5743d9edf357337d7e1f4d9611
MiaoR1Zibing <2691610276@qq.com>
2025-07-12 11:18:34 +0800

    feat: 新增缇宝小火箭对5总伤

--- resources/meta-sr/character/缇宝/calc.js
+++ resources/meta-sr/character/缇宝/calc.js
@@ -3,7 +3,6 @@ export const details = [{
   dmg: ({ talent, calc, attr }, { basic }) => basic(calc(attr.hp) * talent.a['技能伤害'], 'a')
 }, {
   title: '天赋追加攻击伤害',
-  params: { cons6: true },
   dmg: ({ talent, calc, attr }, { basic }) => basic(calc(attr.hp) * talent.t['追加攻击伤害'], 't')
 }, {
   title: '终结技伤害',
@@ -14,9 +13,33 @@ export const details = [{
     let num = cons > 1 ? 1.2 : 1
     return basic(calc(attr.hp) * talent.q['附加伤害'] * num, 'q')
   }
+}, {
+  title: '`起飞!`对5总伤',
+  dmg: ({ talent, cons, calc, attr }, { basic }) => {
+    let addiAttack = basic(calc(attr.hp) * talent.t['追加攻击伤害'], 't')
+    let _ratio = cons > 1 ? 1.2 : 1
+    let num = cons > 1 ? 6 : 5
+    let attaAttack = basic(calc(attr.hp) * talent.q['附加伤害'] * _ratio * num, 'q')
+    let numavg = addiAttack.avg * 5 + attaAttack.avg
+    let numdmg = addiAttack.dmg * 5 + attaAttack.dmg
+    if (cons > 0){
+      numavg *= 1.24
+      numdmg *= 1.24
+    }
+    return {
+      avg: numavg,
+      dmg: numdmg
+    }
+  }
+}, {
+  title: '生命上限提高参考值(4人)',
+  dmg: ({ calc, attr }, { basic }) => {
+    let _hpplus = calc(attr.hp) * 9 * 3 / 100
+    return {avg: _hpplus}
+  }
 }]
 
-export const defDmgIdx = 1
+export const defDmgIdx = 4
 export const mainAttr = 'hp,cpct,cdmg'
 
 export const buffs = [{
@@ -36,10 +59,10 @@ export const buffs = [{
     dmg: 72 * 3
   }
 }, {
-  title: '行迹-长翅膀的玻璃球！：缇宝的生命上限提高，提高数值等同于我方全体角色生命上限之和的9%',
+  title: '行迹-长翅膀的玻璃球！：缇宝的生命上限提高，提高数值等同于我方全体角色生命上限之和的9%(四角色)',
   tree: 2,
   data: {
-    hpPlus: ({ calc, attr }) => calc(attr.hp) * 9 / 100
+    hpPlus: ({ calc, attr }) => calc(attr.hp) * 9 * 3 / 100  // 假设缇宝血量6000, 其他3个队友血量4000, 那么总血量:缇宝血量=3:1 
   }
 }, {
   title: '缇宝4魂：【神启】持续期间，我方全体造成伤害时无视目标[ignore]%的防御',
@@ -55,4 +78,4 @@ export const buffs = [{
   }
 }]
 
-export const createdBy = '冰翼'
+export const createdBy = '冰翼 & 77惨惨77'
